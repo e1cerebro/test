2879bcee41f9b7d11253ccd997f033218fc3b865
nwachuku <nwachukwu16@hotmail.com>
2018-05-05 14:48:08 -0400

    Bugs Fixed 5/5/18

--- src/components/About.vue
+++ src/components/About.vue
@@ -6,7 +6,7 @@
                 <div class="row">
                       <div class="col-md-5">
                        <div class="author-image">
-                            <img src="src/appAssets/img/author.jpg" alt="Author Image"> <!--=== author image ===-->
+                            <img src="static/img/author.jpg" alt="Author Image"> <!--=== author image ===-->
                         </div>
                     </div>
 
@@ -14,7 +14,7 @@
                         <div class="about-text">
                             <h4>I'm a creative <span class="theme-color">Web Developer</span> / <span class="theme-color">WordPress Developer</span></h4>
                             <p>My name is Christian and I'm a Web Developer/WordPress Developer living and working in Windsor, Ontario. I've been designing and developing websites professionally for over 5 years. My goal is to help my clients grow their online presence through unique and inventive web solutions. Please explore my website for past work  and services. If you're ready to discuss your project click on the link below.</p>
-                            <a class="btn theme-color" download="" href="src/appAssets/Christian Nwachukwu Resume.doc">download resume</a>
+                            <a class="btn theme-color" download="" href="static/Christian Nwachukwu Resume.doc">download resume</a>
                             <a class="btn extra smoth-scroll" href="#contact">Get In Touch</a>
                         </div>
                     </div>
